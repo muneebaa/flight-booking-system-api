7de6be37a94a115eaac01ee36e04c6c2687e1061
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2022-10-26 17:06:13 +0500

    httpOnly to true

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,7 +15,7 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
-    httpOnly: false,
+    httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,

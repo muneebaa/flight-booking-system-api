ee1940660b2ebf64788867e5b0aaa8db327f6eaf
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2022-10-27 13:53:40 +0500

    some changes

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,7 +15,7 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
-    httpOnly: true,
+    httpOnly: false,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,

9c4ef1ddb3312ef32e25033e1140d9d6d77879c4
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2022-10-27 11:58:56 +0500

    headers added

--- app.js
+++ app.js
@@ -63,8 +63,17 @@ app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/departureFlights', departureFlightRouter);
 app.use('/api/v1/bookflight', bookFlightRouter);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
   next();
 });
 
